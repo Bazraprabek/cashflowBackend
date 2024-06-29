45a12ba0ee6052f96c589e180ae0d34f305e8b92
siddhartha0 <siddhartha.sunuwar@gmail.com>
2024-06-29 12:17:53 +0545

    midd

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,6 +7,11 @@ const router = express.Router();
 
 router.post("/", UserController.createuser);
 router.get("/", UserController.getAllUser);
+router
+  .route("/:id")
+  .get(UserController.getUserById)
+  .put(UserController.updateUser)
+  .delete(UserController.deleteUser);
 router.post("/login", authController.login);
 router.post("/forgot-password", authController.forgotPassword);
 
