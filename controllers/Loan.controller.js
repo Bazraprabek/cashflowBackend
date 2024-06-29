891b6109237f657d2c6e98dbd022f3e7d56cac7d
Luja Chitrakar <lujachitrakar156@gmail.com>
2024-06-29 12:57:18 +0545

    fixing models

--- controllers/Loan.controller.js
+++ controllers/Loan.controller.js
@@ -1,58 +1,64 @@
 const Loan = require("../models/LoanModel");
 
-exports.createLoan = async (req, res) => {
-  try {
-    const loan = await Loan.create(req.body);
-    res.status(201).json(loan);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-exports.getAllLoans = async (req, res) => {
-  try {
-    const loans = await Loan.findAll();
-    res.json(loans);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-exports.getLoanById = async (req, res) => {
-  try {
-    const loan = await Loan.findById(req.params.id);
-    if (loan) {
-      res.json(loan);
-    } else {
-      res.status(404).json({ message: "Loan not found" });
+class LoanController {
+  async createLoan(req, res) {
+    try {
+      const loan = await Loan.create(req.body);
+      res.status(201).json(loan);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
   }
-};
 
-exports.updateLoan = async (req, res) => {
-  try {
-    const loan = await Loan.update(req.params.id, req.body);
-    if (loan) {
-      res.json(loan);
-    } else {
-      res.status(404).json({ message: "Loan not found" });
+  async getAllLoans(req, res) {
+    try {
+      const loans = await Loan.findAll();
+      res.json(loans);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
   }
-};
 
-exports.deleteLoan = async (req, res) => {
-  try {
-    const loan = await Loan.delete(req.params.id);
-    if (loan) {
-      res.status(204).end();
-    } else {
-      res.status(404).json({ message: "Loan not found" });
+  async getLoanById(req, res) {
+    try {
+      const loan = await Loan.findByPk(req.params.id);
+      if (loan) {
+        res.json(loan);
+      } else {
+        res.status(404).json({ message: "Loan not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
   }
-};
+
+  async updateLoan(req, res) {
+    try {
+      const loan = await Loan.findByPk(req.params.id);
+      if (loan) {
+        await loan.update(req.body);
+        res.json(loan);
+      } else {
+        res.status(404).json({ message: "Loan not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async deleteLoan(req, res) {
+    try {
+      const loan = await Loan.findByPk(req.params.id);
+      if (loan) {
+        await loan.destroy();
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: "Loan not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
+
+module.exports = new LoanController();
--- models/LoanModel.js
+++ models/LoanModel.js
@@ -1,40 +1,31 @@
-const pool = require("sequelize");
+const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../config/db");
 
-class Loan {
-  static async create({ borrower, amount, interestRate, duration }) {
-    const result = await pool.query(
-      "INSERT INTO loans (borrower, amount, interest_rate, duration) VALUES ($1, $2, $3, $4) RETURNING *",
-      [borrower, amount, interestRate, duration]
-    );
-    return result.rows[0];
-  }
+class Loan extends Model {}
 
-  static async findAll() {
-    const result = await pool.query("SELECT * FROM loans");
-    return result.rows;
+Loan.init(
+  {
+    borrower: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    interestRate: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Loan",
   }
-
-  static async findById(id) {
-    const result = await pool.query("SELECT * FROM loans WHERE id = $1", [id]);
-    return result.rows[0];
-  }
-
-  static async update(id, { borrower, amount, interestRate, duration }) {
-    const result = await pool.query(
-      "UPDATE loans SET borrower = $1, amount = $2, interest_rate = $3, duration = $4 WHERE id = $5 RETURNING *",
-      [borrower, amount, interestRate, duration, id]
-    );
-    return result.rows[0];
-  }
-
-  static async delete(id) {
-    const result = await pool.query(
-      "DELETE FROM loans WHERE id = $1 RETURNING *",
-      [id]
-    );
-    return result.rows[0];
-  }
-}
+);
 
 module.exports = Loan;
