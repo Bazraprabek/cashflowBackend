3d507499cbc53377ddcba982e31358db774248eb
bazraprbek <bazraprbek@gmail.com>
2024-06-29 20:40:46 +0545

    minor changes

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -8,7 +8,6 @@ class Auth {
     if (token) {
       let userToken = token.split(" ")[1];
       const decode = verifyToken(userToken);
-
       if (decode.role === "admin") next();
       else {
         return next(new AppError("You are not authorized"), 400);
@@ -22,21 +21,31 @@ class Auth {
     const token = req.headers.authorization;
     if (token) {
       let userToken = token.split(" ")[1];
-      const decode = verifyToken(userToken);
-      if (decode) {
+      try {
+        const decode = verifyToken(userToken);
+        console.log(decode);
+
         const findUser = await User.findOne({
           where: { username: decode.username },
         });
+
         if (findUser) {
+          req.user = decode.id;
           next();
         } else {
-          return next(new AppError("Please sign in your account"), 400);
+          return next(new AppError("Please sign in to your account.", 400));
         }
-      } else {
-        return next(new AppError("You are not authorized"), 400);
+      } catch (error) {
+        if (error.name === "TokenExpiredError") {
+          return next(
+            new AppError("Your session has expired. Please sign in again.", 401)
+          );
+        } else {
+          return next(new AppError("You are not authorized", 401));
+        }
       }
     } else {
-      return next(new AppError("Please authorized first", 404));
+      return next(new AppError("Please authorize first", 404));
     }
   }
 }
